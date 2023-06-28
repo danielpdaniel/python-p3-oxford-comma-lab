13f8fb4bdfb075f0a0c0dd7adfeeb3916901d8e6
danielpdaniel <artofjquinn@gmail.com>
2023-06-27 20:56:34 -0400

    Update function to get tests passing

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    length = len(items)
+    if length == 2:
+        return " and ".join(items)
+    elif length == 1:
+        return "".join(items)
+    else:
+       return  ", ".join(items[0:length - 1]) + f", and {items[length - 1]}"
